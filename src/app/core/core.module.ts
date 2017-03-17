adbd4dcefdce2d9b1fa3a7cf49241da9aa6e0f8b
Wanasak <u510610433@gmail.com>
2017-03-17 15:55:16 +0700

    add navbar module component into core module #Subscription

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { GrowlerModule } from './growler/growler.module';
 import { ModalModule } from './modal/modal.module';
 
+/* Services */
+import { AuthService } from './services/auth.service';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
 import { EnsureModuleLoadedOnceGuard } from './ensureModuleLoadedOnceGuard';    // Prevent reimport of the CoreModule
 
 @NgModule({
     imports: [CommonModule, GrowlerModule, ModalModule],
-    exports: [GrowlerModule, ModalModule]
+    exports: [GrowlerModule, ModalModule, NavbarComponent],
+    declarations: [NavbarComponent],
+    providers: [AuthService]    // This should be singleton
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
 
---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';   // Subscription - Represent a disposable resource
+
+import { AuthService } from '../services/auth.service';
+import { GrowlerService, GrowlerMessageType } from '../growler/growler.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-navbar',
+    templateUrl: 'navbar.component.html'
+})
+export class NavbarComponent implements OnInit, OnDestroy {
+
+    isCollapsed: boolean;
+    loginLogoutText: string = 'Login';
+    sub: Subscription;
+
+    constructor(
+        private router: Router,
+        private authService: AuthService,
+        private growler: GrowlerService
+    ) { }
+
+    ngOnInit() {
+        this.sub = this.authService.authChanged
+            .subscribe((loggedIn: boolean) => {
+                this.setLoginLogoutText();
+            }, (err: any) => console.error(err));
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    loginOrLogout() {
+        const isAuthenticated = this.authService.isAuthenticated;
+        if (isAuthenticated) {
+            this.authService.logout()
+                .subscribe((status: boolean) => {
+                    this.setLoginLogoutText();
+                    this.growler.growl('Logged Out', GrowlerMessageType.Info);
+                    this.router.navigate(['/customer']);
+                }, (err: any) => console.error(err));
+        }
+        this.redirectToLogin();
+    }
+
+    redirectToLogin() {
+        this.router.navigate(['/login']);
+    }
+
+    setLoginLogoutText() {
+        this.loginLogoutText = (this.authService.isAuthenticated) ? 'Logout' : 'Login';
+    }
+
+}
