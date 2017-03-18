b1d5726293324efb7aa8bc736496dd88506e166a
Wanasak <u510610433@gmail.com>
2017-03-18 20:54:36 +0700

    add capitalize pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TrimPipe } from './pipes/trim.pipe';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
     imports: [CommonModule],
-    exports: [CommonModule, TrimPipe],
-    declarations: [TrimPipe]
+    exports: [CommonModule, TrimPipe, CapitalizePipe],
+    declarations: [TrimPipe, CapitalizePipe]
 })
 export class SharedModule { }
