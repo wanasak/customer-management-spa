7636dbfca8b1c3691f0856eab43093ff628e1a62
Wanasak <u510610433@gmail.com>
2017-03-19 14:01:08 +0700

    add pagination module into shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FilterTextboxModule } from './filter-textbox/filter-textbox.module';
+import { PaginationModule } from './pagination/pagination.module';
 
 import { TrimPipe } from './pipes/trim.pipe';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
@@ -9,8 +10,8 @@ import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { SortByDirective } from './directives/sortby.directive';
 
 @NgModule({
-    imports: [CommonModule, FilterTextboxModule],
-    exports: [CommonModule, TrimPipe, CapitalizePipe, SortByDirective, FilterTextboxModule],
+    imports: [CommonModule, FilterTextboxModule, PaginationModule],
+    exports: [CommonModule, TrimPipe, CapitalizePipe, SortByDirective, FilterTextboxModule, PaginationModule],
     declarations: [TrimPipe, CapitalizePipe, SortByDirective]
 })
 export class SharedModule { }
