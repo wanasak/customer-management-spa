79d980f95980777c92e72ecad6c70f03d9710256
Wanasak <u510610433@gmail.com>
2017-03-31 00:51:20 +0700

    fixed bug

--- src/app/shared/filter-textbox/filter-textbox.module.ts
+++ src/app/shared/filter-textbox/filter-textbox.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { FilterTextboxComponent } from './filter-textbox.component';
+
 @NgModule({
     imports: [CommonModule, FormsModule],
-    exports: [],
-    declarations: []
+    exports: [FilterTextboxComponent],
+    declarations: [FilterTextboxComponent]
 })
 export class FilterTextboxModule { }
